cf36625f3f1d1e7f131b2ba27deed17c57e07fac
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-06-15 14:47:41 -0300

    Login page done

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:simple_login/Screens/Login/components/background.dart';
+import 'package:simple_login/components/already_have_an_account_check.dart';
+import 'package:simple_login/components/rounded_button.dart';
+import 'package:simple_login/components/rounded_input_field.dart';
+import 'package:simple_login/components/rounded_password_field.dart';
+import 'package:simple_login/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -8,28 +15,37 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      width: double.infinity,
-      height: size.height,
-      child: Stack(
-        alignment: Alignment.center,
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.35,
+          Text(
+            "LOGIN",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
           ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Image.asset(
-              "assets/images/login_bottom.png",
-              width: size.width * 0.4,
-            ),
-          )
+          SizedBox(height: size.height * 0.03),
+          SvgPicture.asset(
+            "assets/icons/login.svg",
+            height: size.height * 0.35,
+          ),
+          SizedBox(height: size.height * 0.03),
+          RoundedInputField(
+            hintText: "Your Email",
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          RoundedButton(
+            text: "LOGIN",
+            press: () {},
+          ),
+          SizedBox(height: size.height * 0.03),
+          AlreadyHaveAnAccountCheck(
+            press: () {},
+          ),
         ],
       ),
     );
